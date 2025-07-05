f16db921f5c580b4423b31b39d5c08da4f209c88
Rutikasri <rutikasripotnuru20@gmail.com>
2025-07-05 10:36:24 +0530

    first commit

---
+++ greatestof3numbers.py
@@ -0,0 +1,8 @@
+nums = input("Enter three numbers separated by spaces: ").split()
+a,b,c =map(int,nums)
+greatest = max(a,b,c)
+print(greatest)
+
+# Enter three numbers separated by spaces: 10 20 30
+# 30
+
